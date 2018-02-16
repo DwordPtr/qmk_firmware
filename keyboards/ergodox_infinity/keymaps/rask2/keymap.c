0e34c30bf97c29545fdb84a7d7aa332185c95711
Bryan Tidwell <btidwell4@my.apsu.edu>
2018-02-15 21:32:43 -0600

    fix backwards braces

--- keyboards/ergodox_infinity/keymaps/rask2/keymap.c
+++ keyboards/ergodox_infinity/keymaps/rask2/keymap.c
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_RBRC,KC_LBRC, KC_LCBR, KC_RCBR, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_LBRC,KC_RBRC, KC_LCBR, KC_RCBR, KC_TRNS,
                 KC_LEFT, KC_DOWN,KC_UP, KC_RIGHT, KC_TRNS, KC_TRNS,
        KC_PIPE, KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS,
                          KC_TRNS,KC_TRNS, KC_HOME, KC_PGDN, KC_END,
