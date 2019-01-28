0f3875a567af0755f53a81d60aa4433d069a40ab
Bryan Tidwell <btidwell@atiba.com>
2019-01-28 14:53:49 -0600

    keep infinity rask2 in sync with ez

--- keyboards/ergodox_infinity/keymaps/rask2/keymap.c
+++ keyboards/ergodox_infinity/keymaps/rask2/keymap.c
@@ -11,7 +11,9 @@ enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
   EPRM,
   VRSN,
-  RGB_SLD
+  RGB_SLD,
+  WHPTXT,
+  CMTTXT
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -94,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
        KC_TRNS, KC_TRNS, KC_LBRC,KC_RBRC, KC_LCBR, KC_RCBR, KC_TRNS,
                 KC_LEFT, KC_DOWN,KC_UP, KC_RIGHT, KC_TRNS, KC_TRNS,
-       KC_PIPE, KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS,
+       KC_PIPE, KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, KC_PGUP, CMTTXT,
                          KC_TRNS,KC_TRNS, KC_HOME, KC_PGDN, KC_END,
        KC_TRNS,   KC_TRNS,
        KC_TRNS,
@@ -124,8 +126,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // MEDIA AND MOUSE
 [FL2] = LAYOUT_ergodox(
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_CAPS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_BTN1, KC_BTN2, KC_TRNS,
+       KC_CAPS, WHPTXT, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        EPRM,    KC_APP,  KC_TRNS, KC_BTN1, KC_BTN2,
                                            KC_TRNS, KC_TRNS,
@@ -188,6 +190,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case WHPTXT:
+      if(record->event.pressed) {
+           SEND_STRING(SS_LGUI("a")); 
+	   SEND_STRING("\b");
+      }
+      return false;
+      break;
+      /*macro to make nerdcommenter more intellij like*/
+     case CMTTXT:
+      if(record->event.pressed) {
+           SEND_STRING("\\"); 
+           SEND_STRING("c"); 
+	   SEND_STRING(" ");
+      }
+      return false;
+      break;
+
   }
   return true;
 }
