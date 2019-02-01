6b5400dd569d4d417682949f5b739d5f5597367c
Bryan Tidwell <btidwell@atiba.com>
2019-02-01 12:16:29 -0600

    use arrow keys for other stuff and bring back o/p + fn1 tab switching

--- keyboards/ergodox_ez/keymaps/rask2/keymap.c
+++ keyboards/ergodox_ez/keymaps/rask2/keymap.c
@@ -59,8 +59,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_DEL,
              KC_RBRC,KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
                      KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
-             KC_RSFT,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_UP,  KC_SLSH,
-                             KC_RALT,MO(FL2),KC_LEFT,KC_DOWN,KC_RIGHT,
+             KC_RSFT,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_BSLS,  KC_SLSH,
+                             KC_RALT,MO(FL2),KC_LEFT_PAREN,KC_DOWN,KC_RIGHT_PAREN,
              KC_RCTL,        KC_ESC,
              KC_PGUP,
              KC_PGDN,KC_ENT, KC_SPC
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, PAREN, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_TRNS,
                 KC_LEFT, KC_DOWN,KC_UP,   KC_RIGHT, KC_ESC, KC_TRNS,
        KC_PIPE, KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, KC_PGUP, CMTTXT,
                          KC_TRNS,KC_TRNS, KC_HOME, KC_PGDN, KC_END,
