f894cd252cf4744048cb61160c6d6b0bed7f44fb
Bryan Tidwell <btidwell@atiba.com>
2019-03-20 15:18:47 -0500

    make -> right control

--- keyboards/ergodox_ez/keymaps/rask2/keymap.c
+++ keyboards/ergodox_ez/keymaps/rask2/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_RBRC,KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
                      KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
              KC_RSFT,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_UP,  KC_SLSH,
-                             KC_RALT,MO(FL2),KC_LEFT,KC_DOWN,KC_RIGHT,
+                             KC_RALT,MO(FL2),KC_LEFT,KC_DOWN,KC_RCTL,
              KC_RCTL,        KC_ESC,
              KC_PGUP,
              KC_ESC,KC_ENT, KC_SPC
