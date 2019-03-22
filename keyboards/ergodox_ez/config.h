2cb2b2a880eafd6ee830650bbd4ab59bbfa05d30
Bryan Tidwell <btidwell@atiba.com>
2019-03-21 21:34:12 -0500

    add toggled layer modifiers so they can still do work 🎉

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -21,6 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
+#define PERMISSIVE_HOLD
+
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x1307
--- keyboards/ergodox_ez/keymaps/rask2/keymap.c
+++ keyboards/ergodox_ez/keymaps/rask2/keymap.c
@@ -72,8 +72,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
-        MO(FL4) ,KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,
-        MO(FL1),KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
+        LT(FL4,KC_TAB) ,KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,
+        LT(FL1,KC_ESC),KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_BSLS,
         KC_LCTL,TG(FL3),KC_LGUI,KC_END,KC_LALT,
                                         KC_INS, KC_PSCR,
