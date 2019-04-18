86fd3c7a7ec910bdf6fa4facb39d04703bfcfd64
Bryan Tidwell <btidwell@atiba.com>
2019-04-18 13:02:09 -0500

    ensure base layer has forward slash

--- keyboards/ergodox_ez/keymaps/rask2/keymap.c
+++ keyboards/ergodox_ez/keymaps/rask2/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                         KC_DEL,
                                         KC_SPC, KC_BSPC, KC_TAB,
         // right hand
-             KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_DEL,
+             KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_SLASH,
              KC_RBRC,KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
                      KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
              KC_BSLS,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_UP,  KC_RSHIFT,
